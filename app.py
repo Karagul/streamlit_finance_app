b43f01c244be4e2ca25fea15f9c67518d8aa17f2
SirVectrex <43294797+SirVectrex@users.noreply.github.com>
2020-08-01 19:48:09 +0200

    YFinance Update

--- app.py
+++ app.py
@@ -1,3 +1,47 @@
+import yfinance as yf
 import streamlit as st
+import json
+from datetime import date
 
-st.title("Hello guys")
+st.write("""
+# Simple Stock Price App
+Closing price per day is shown. 1 Dataset per day.
+""")
+
+#user_input = st.text_input("Stock", "GOOGL")
+#startDate = st.date_input("Beginn of Timeframe")
+#endDate = st.date_input("End of Timeframe")
+
+#sidebar with input parameters
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    stringinput = st.sidebar.text_input("Stock", "GOOGL")
+    startDate2 = st.sidebar.date_input("Beginn of Timeframe", date.fromisoformat('2010-05-31'))
+    endDate2 = st.sidebar.date_input("End of Timeframe", date.fromisoformat('2020-05-31'))
+    return stringinput
+
+user_input = user_input_features()
+
+
+
+#define the ticker symbol
+stockname = user_input
+#get data on this ticker
+tickerData = yf.Ticker(stockname)
+#st.write(yf.Ticker(stockname))
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+#st.write(tickerDf.empty )
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+if tickerDf.empty == True :
+    st.write("""No Data found for entry. Please enter another stock-identifyer""")
+else:
+    st.write("Stock Name **", str(tickerData.info["longName"]), "**")
+    st.write("Current Ask Price **",  str(tickerData.info["ask"]), "**")
+    st.write("""Curreny:""", str(tickerData.info["currency"]))
+    st.write("""Closure Values""")
+    st.line_chart(tickerDf.Close)
+    st.write("""Volume of Stock""")
+    st.line_chart(tickerDf.Volume)
