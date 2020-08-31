37916905fb662ab1385b19a98595923554841bad
SirVectrex <florian.schulenberg@web.de>
2020-08-31 14:41:29 +0200

    input now handled through array

--- app.py
+++ app.py
@@ -16,32 +16,38 @@ Closing price per day is shown. 1 Dataset per day.
 # sidebar with input parameters
 st.sidebar.header('User Input Parameters')
 
+def read_saved_stocks():
+    file = open("mystocks.txt", "r")
+    print(f.read())
 
 def user_input_features():
-    stringinput = st.sidebar.text_input("Stock", "GOOGL")
-    return stringinput
+    stocksymbol = st.sidebar.text_input("Stock", "GOOGL")
+    startDate = st.sidebar.date_input("Beginn of Timeframe", datetime.date(2010, 5, 30))
+    endDate = st.sidebar.date_input("End of Timeframe", datetime.date.today())
+    user_input_array = [stocksymbol, startDate, endDate]
+    return user_input_array
 
 
 user_input = user_input_features()
 
 # define the ticker symbol
-stockname = user_input
+stockname = user_input[0]
 # get data on this ticker
 tickerData = yf.Ticker(stockname)
 # st.write(yf.Ticker(stockname))
 # get the historical prices for this ticker
-startdate = st.sidebar.date_input("Beginn of Timeframe", datetime.date(2010, 5, 31))
-endDate = st.sidebar.date_input("End of Timeframe", datetime.date.today())
+user_startdate = user_input[1]
+user_enddate = user_input[2]
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
-tickerDf = tickerData.history(period='1d', start= startdate, end= endDate)
+tickerDf = tickerData.history(period='1d', start=user_startdate, end=user_enddate)
 # st.write(tickerDf.empty )
 
 if tickerDf.empty:
     st.write("""No Data found for entry. Please enter another stock-identifyer""")
 else:
     st.write("Stock Name **", str(tickerData.info["longName"]), "**")
-    st.write("Current Ask Price **",  str(tickerData.info["ask"]), "**", str(tickerData.info["currency"]))
-    #st.write("""Curreny:""", str(tickerData.info["currency"]))
+    st.write("Current Ask Price **", str(tickerData.info["ask"]), "**", str(tickerData.info["currency"]))
+    # st.write("""Curreny:""", str(tickerData.info["currency"]))
     st.write("""Closure Values""")
     st.line_chart(tickerDf.Close)
     st.write("""Volume of Stock""")
